13634a46eda426840b25cc4e6d84c1f5066f857c
Pepper0ni <pepper0ni3steps@gmail.com>
2022-07-22 20:46:03 +0100

    react better to scaling, other optimisations

--- PackSpreader.lua
+++ PackSpreader.lua
@@ -1,37 +1,34 @@
-function onLoad()
- curColumn=9
- local selfScale=self.getScale()
+function onLoad(state)
+ curColumn=tonumber(state)or 9
  local params={
- function_owner=self,
- label='Spread Compact',
- tooltip="Spreads the contained objects close together",
- font_size=180,
- width=1500,
- height=220,
- scale={1/selfScale.x,1/selfScale.y,1/selfScale.z},
- position={0,1,2.5},
- click_function='SpreadCompact'
+  function_owner=self,
+  font_size=180,
+  width=1500,
+  height=220,
+  position={0,1,3.5},
+  tooltip="The number of packs per column",
+  value=tostring(curColumn),
+  alignment=3,
+  input_function="SetColumn",
+  font_color={0,0,0},
  }
- self.createButton(params)
-
- params.position[3]=3
- params.label='Spread Wide'
- params.tooltip="Spreads the packs wide enough to not collide when opening."
- params.click_function='SpreadWide'
- self.createButton(params)
-
- params.position[3]=3.5
- params.tooltip="The number of packs per column"
- params.value=tostring(curColumn)
- params.alignment=3
- params.input_function="SetColumn"
- params.font_color={0,0,0}
  self.createInput(params)
+ butWrapper(params,{0,1,2.5},"Spread Compact","Spreads the contained objects close together","SpreadCompact")
+ butWrapper(params,{0,1,3},"Spread Wide","Spreads the packs wide enough to not collide when opening.","SpreadWide")
+end
+
+function butWrapper(params,pos,label,tool,func)
+ params.position=pos
+ params.label=label
+ params.tooltip=tool
+ params.click_function=func
+ self.createButton(params)
 end
 
 function SetColumn(obj,color,value,selected)
  if not selected then
   curColumn=checkIfNum(value,1000,curColumn,color)
+  self.script_state=tostring(curColumn)
   return tostring(curColumn)
  end
 end
